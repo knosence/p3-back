e7a2537ae55476c285422b3a78b0a6e14e36384e
Knosence <nadario.seays@gmail.com>
2023-01-24 16:12:21 -0500

    wiping to reset

--- warehouse-api/src/main/java/com/skillstrom/projectthree/warehouseapi/item/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillstrom.projectthree.warehouseapi.item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-
-}
--- warehouse-api/src/main/java/com/skillstrom/projectthree/warehouseapi/warehouse/WarehouseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillstrom.projectthree.warehouseapi.warehouse;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Integer> {
-
-
-}
