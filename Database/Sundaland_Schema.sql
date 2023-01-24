b7d1c15e0088867b3a51e319d9df5c689f5fcfdb
Knosence <nadario.seays@gmail.com>
2023-01-24 15:46:58 -0500

    repushing

--- Database/Sundaland_Schema.sql
+++ Database/Sundaland_Schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE sundaland;
 USE sundaland;
 
 CREATE TABLE location (
-	location_id INT NOT NULL PRIMARY KEY,
+	location_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
 	street_address VARCHAR(60) NOT NULL,
 	city VARCHAR(40) NOT NULL,
 	state CHAR(2) NOT NULL,
@@ -12,26 +12,28 @@ CREATE TABLE location (
 );
 
 CREATE TABLE company (
-    company_id INT NOT NULL PRIMARY KEY,
+    company_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     company_name VARCHAR(40) NOT NULL,
    	location_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES location(location_id)
+    CONSTRAINT FK_CompanyLocation FOREIGN KEY (location_id) REFERENCES location(location_id)
 
 );
 
 CREATE TABLE warehouse (
-    warehouse_id INT NOT NULL PRIMARY KEY,
+    warehouse_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     location_id INT NOT NULL,
-    item_size INT NOT NULL,
-    company_id INT,
+    warehouse_capacity INT NOT NULL,
+    company_id INT NOT NULL,
+CONSTRAINT FK_CompanyWarehouse
     FOREIGN KEY (company_id)
         REFERENCES company(company_id),
+CONSTRAINT FK_WarehouseLocation
     FOREIGN KEY (location_id)
-        REFERENCES location(location_id)
+        REFERENCES location(location_id),
 );
 
 CREATE TABLE item (
-	item_id INT NOT NULL PRIMARY KEY,
+	item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	item_name VARCHAR(40) NOT NULL,
 	item_description VARCHAR(255) NOT NULL,
 	item_size TINYINT NOT NULL
@@ -39,21 +41,25 @@ CREATE TABLE item (
 
 CREATE TABLE warehouse_inventory (
 	item_id INT NOT NULL,
+	inventory_id INT NOT NULL,
 	warehouse_id INT NOT NULL,
 	item_quantity INT NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES item(item_id),
+CONSTRAINT FK_InventoryItem
+	FOREIGN KEY (item_id) REFERENCES item(item_id),
+CONSTRAINT FK_WarehouseInventory
 	FOREIGN KEY (warehouse_id) REFERENCES warehouse(warehouse_id)
 
 );
 
 CREATE TABLE employee (
-    employee_id INT PRIMARY KEY NOT NULL,
+    employee_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(20) NOT NULL,
     Last_name VARCHAR(40) NOT NULL,
     warehouse_id INT NOT NULL,
     employee_email VARCHAR(50) NOT NULL,
     employee_password VARCHAR(30) NOT NULL,
     user_role VARCHAR(20) NOT NULL,
+CONSTRAINT FK_WarehouseEmployee
     FOREIGN KEY (warehouse_id)
         REFERENCES warehouse (warehouse_id)
 );
