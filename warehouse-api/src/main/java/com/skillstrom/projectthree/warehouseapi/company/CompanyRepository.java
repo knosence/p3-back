6a4e4e1a78a53bab26c534be37bf692ed8150f16
Knosence <nadario.seays@gmail.com>
2023-01-24 16:17:35 -0500

    basing

--- warehouse-api/src/main/java/com/skillstrom/projectthree/warehouseapi/company/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillstrom.projectthree.warehouseapi.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
-
-
-}
